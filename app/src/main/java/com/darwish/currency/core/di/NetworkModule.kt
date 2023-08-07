4a97aa7d46a49390db28609c15c38163b23498a6
darwish <darwish1994@outlook.com>
2023-08-07 20:54:40 +0300

    create di for network model

---
+++ app/src/main/java/com/darwish/currency/core/di/NetworkModule.kt
@@ -0,0 +1,60 @@
+package com.darwish.currency.core.di
+
+import com.darwish.currency.BuildConfig
+import com.darwish.currency.core.network.NetworkHelper
+import com.darwish.currency.core.network.ResponseInterceptor
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient, gson: Gson): Retrofit {
+
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideOKHTTPInstance(interceptor: ResponseInterceptor): OkHttpClient {
+        val httpClient = OkHttpClient().newBuilder()
+            .addInterceptor(interceptor)
+
+        if (BuildConfig.DEBUG) {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            httpClient.addInterceptor(loggingInterceptor).build()
+        }
+
+        return httpClient.build()
+    }
+    @Provides
+    @Singleton
+    fun provideInterceptor(networkHelper: NetworkHelper)= ResponseInterceptor(networkHelper)
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create()
+
+
+
+}
