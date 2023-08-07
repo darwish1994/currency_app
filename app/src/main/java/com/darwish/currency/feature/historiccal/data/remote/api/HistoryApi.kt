1171d6889e31c7975d50624cce5fa02fc70cc57c
darwish <darwish1994@outlook.com>
2023-08-07 21:02:07 +0300

    create history api interface

---
+++ app/src/main/java/com/darwish/currency/feature/historiccal/data/remote/api/HistoryApi.kt
@@ -0,0 +1,17 @@
+package com.darwish.currency.feature.historiccal.data.remote.api
+
+import com.darwish.currency.core.network.ApiUrl
+import com.darwish.currency.feature.historiccal.data.remote.response.CurrencySeriesResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface HistoryApi {
+
+    @GET(ApiUrl.history)
+    suspend fun getTimeSeries(
+        @Query("start_date") startDate: String,
+        @Query("end_date") endDate: String,
+        @Query("base") from: String,
+        @Query("symbols") to: String
+    ): CurrencySeriesResponse
+}
