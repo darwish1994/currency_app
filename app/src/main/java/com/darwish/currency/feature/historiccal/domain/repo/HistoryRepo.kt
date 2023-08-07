fee58ce925b26b6f7c9fed025edb144589453308
darwish <darwish1994@outlook.com>
2023-08-07 21:24:18 +0300

    add fun to get popular currencies rate

--- app/src/main/java/com/darwish/currency/feature/historiccal/domain/repo/HistoryRepo.kt
+++ app/src/main/java/com/darwish/currency/feature/historiccal/domain/repo/HistoryRepo.kt
@@ -6,6 +6,7 @@ import com.darwish.currency.core.network.Resource
 interface HistoryRepo {
 
     suspend fun getCurrencyHistory(from:String,to:String,lastDays:Int):Resource<List<CurrencyRate>>
+    suspend fun getOtherCurrencyRate(base:String,popularCurrencies:List<String>):Resource<List<CurrencyRate>>
 
 
 }
